593bf21886d1e3321220aef3cd07da3308ad1238
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-09-28 15:22:24 -0400

    Move password in environment variable to avoid having them in log

--- modules/freeipa/files/ipa_create_user.sh
+++ modules/freeipa/files/ipa_create_user.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-ADMIN_PASSWD=$1
-USERNAME=$2
-PASSWORD=$3
 
-echo $ADMIN_PASSWD | kinit admin
-echo $PASSWORD | ipa user-add $USERNAME --first "$USERNAME" --last "$USERNAME" --cn "$USERNAME" --shell /bin/bash --password
+USERNAME=$1
+if [ -z "${IPA_ADMIN_PASSWD+xxx}" ];
+    echo "Please enter your FreeIPA admin password: "
+    read -sr IPA_PASSWORD_INPUT
+    IPA_ADMIN_PASSWD="$IPA_PASSWORD_INPUT"
+fi
+if [ -z "${IPA_GUEST_PASSWD+xxx}" ];
+    echo "Please enter the guest password: "
+    read -sr GUEST_PASSWORD_INPUT
+    IPA_GUEST_PASSWD="$GUEST_PASSWORD_INPUT"
+fi
+
+echo $IPA_ADMIN_PASSWD | kinit admin
+echo $IPA_GUEST_PASSWD | ipa user-add $USERNAME --first "-" --last "-" --cn "$USERNAME" --shell /bin/bash --password
 kdestroy
-echo -e "$PASSWORD\n$PASSWORD\n$PASSWORD" | kinit $USERNAME
+echo -e "$IPA_GUEST_PASSWD\n$IPA_GUEST_PASSWD\n$IPA_GUEST_PASSWD" | kinit $USERNAME
 kdestroy
 mkhomedir_helper $USERNAME
