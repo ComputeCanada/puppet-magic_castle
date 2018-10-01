7b862c48781db46ef666a1b2054cac0034b73009
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-10-01 14:52:16 -0400

    Add missing comma

--- site/profile/manifests/cvmfs.pp
+++ site/profile/manifests/cvmfs.pp
@@ -20,7 +20,7 @@ class profile::cvmfs::client (String $squid_server = "mgmt01") {
 
   file { '/etc/cvmfs/default.local':
     ensure  => 'present',
-    content => epp('profile/cvmfs/default.local', { 'squid_server' => $squid_server })
+    content => epp('profile/cvmfs/default.local', { 'squid_server' => $squid_server }),
     require => Package['cvmfs']
   }
 
