c46801397e29ca6dd4898efc2b2d727f55588a6a
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-10-02 15:32:29 -0400

    Make puppet default node a login node

--- site.pp
+++ site.pp
@@ -1,17 +1,11 @@
 node default {
-  include profile::base
-  include profile::nfs::client
-  include profile::rsyslog::client
-}
-
-node /^mgmt\d+$/ {
   require profile::base
-  require profile::freeipa::server
-  include profile::freeipa::guest_accounts
-  include profile::nfs::server
-  include profile::rsyslog::server
-  include profile::squid::server
-  include profile::slurm::controller
+  require profile::freeipa::client
+  include profile::nfs::client
+  include profile::cvmfs::client
+  include profile::rsyslog::client
+  include profile::slurm::submitter
+  include jupyterhub
 }
 
 node /^login\d+$/ {
@@ -22,6 +16,16 @@ node /^login\d+$/ {
   include profile::rsyslog::client
   include profile::slurm::submitter
   include jupyterhub
+}
+
+node /^mgmt\d+$/ {
+  require profile::base
+  require profile::freeipa::server
+  include profile::freeipa::guest_accounts
+  include profile::nfs::server
+  include profile::rsyslog::server
+  include profile::squid::server
+  include profile::slurm::controller
 }
 
 node /^node\d+$/ {
