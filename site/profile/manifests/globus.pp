e3925a9488c6b8d4f19298162671544d937af2fb
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-10-16 11:04:13 -0400

    Fix globus module multiple errors

--- site/profile/manifests/globus.pp
+++ site/profile/manifests/globus.pp
@@ -9,19 +9,20 @@ class profile::globus::base (String $globus_user = '', String $globus_password =
 
   package { 'globus-connect-server':
     ensure  => 'installed',
-    require => Yumrepo['globus-connect-server-repo']
+    require => Package['globus-connect-server-repo']
   }
 
   $domain_name = lookup('profile::freeipa::base::domain_name')
   file { '/etc/globus-connect-server.conf':
     ensure  => 'present',
-    content => epp('profile/globus/globus-connect-server.conf', { 'domain_name' => $::domain_name,
-                                                                  'hostname'    => $::hostname }),
+    content => epp('profile/globus/globus-connect-server.conf', { 'domain_name' => $domain_name,
+                                                                  'hostname'    => $hostname }),
   }
 
-  if ($::globus_user != '') and ($::globus_password != '') {
+  if ($globus_user != '') and ($globus_password != '') {
     exec { '/usr/bin/globus-connect-server-setup':
-      environment => { 'GLOBUS_USER' => $::globus_user, 'GLOBUS_PASSWORD' => $::globus_password},
+      environment => ["GLOBUS_USER=${globus_user}",
+                      "GLOBUS_PASSWORD=${globus_password}"],
     }
   }
 
