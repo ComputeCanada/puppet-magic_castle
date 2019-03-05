767eb0ffcb73b0dad0d3b4c15f57b690afc20e77
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-03-05 10:26:50 -0500

    Fix sponsor in ipa_add_user

--- site/profile/manifests/freeipa.pp
+++ site/profile/manifests/freeipa.pp
@@ -120,7 +120,7 @@ class profile::freeipa::guest_accounts(String $guest_passwd,
   }
 
   exec{ "ipa_add_user":
-    command     => "ipa_create_user.sh ${prefix}{01..${nb_accounts}} sponsor00",
+    command     => "ipa_create_user.sh ${prefix}{01..${nb_accounts}} Sponsor=sponsor00",
     unless      => "test `ls /home | grep ${prefix} | wc -l` == ${nb_accounts}",
     environment => ["IPA_ADMIN_PASSWD=$admin_passwd",
                     "IPA_GUEST_PASSWD=$guest_passwd"],
