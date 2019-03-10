9fcb81cf2639f6a53dc5d00503871426e993da05
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-03-10 14:11:38 -0400

    Update nfs client and server to use /mnt and of /

--- site/profile/manifests/nfs.pp
+++ site/profile/manifests/nfs.pp
@@ -9,17 +9,20 @@ class profile::nfs::client (String $server = "mgmt01") {
     nfs_v4_idmap_domain => $nfs_domain
   }
   selinux::boolean { 'use_nfs_home_dirs': }
-  nfs::client::mount { '/home':
+  nfs::client::mount { '/mnt/home':
       server => $server,
-      share => 'home'
+      share => 'home',
+      mount => '/home'
   }
-  nfs::client::mount { '/project':
+  nfs::client::mount { '/mnt/project':
       server => $server,
-      share => 'project'
+      share => 'project',
+      mount => '/project'
   }
-  nfs::client::mount { '/scratch':
+  nfs::client::mount { '/mnt/scratch':
       server => $server,
-      share => 'scratch'
+      share => 'scratch',
+      mount => '/scratch'
   }
   nfs::client::mount { '/etc/slurm':
       server => $server,
@@ -38,21 +41,21 @@ class profile::nfs::server {
                           default_value => $::domain })
   $nfs_domain  = "int.$domain_name"
 
-  file { ['/project', '/scratch'] :
+  file { ['/mnt/project', '/mnt/scratch'] :
     ensure  => directory,
     seltype => 'usr_t'
   }
 
-  file { ['/project/6002799', '/project/6002799/photos'] :
+  file { ['/mnt/project/6002799', '/mnt/project/6002799/photos'] :
     ensure => directory
   }
 
-  file { '/project/6002799/photos/KSC2018.jpg':
+  file { '/mnt/project/6002799/photos/KSC2018.jpg':
     ensure => 'present',
     source => "https://images-assets.nasa.gov/image/KSC-20180316-PH_JBS01_0118/KSC-20180316-PH_JBS01_0118~orig.JPG"
   }
 
-  file { "/project/6002799/photos/VAFB2018.jpg":
+  file { "/mnt/project/6002799/photos/VAFB2018.jpg":
     ensure => 'present',
     source => "https://images-assets.nasa.gov/image/VAFB-20180302-PH_ANV01_0056/VAFB-20180302-PH_ANV01_0056~orig.jpg"
   }
@@ -66,7 +69,7 @@ class profile::nfs::server {
     nfs_v4_idmap_domain => $nfs_domain
   }
 
-  nfs::server::export{ ['/etc/slurm', '/home', '/project', '/scratch'] :
+  nfs::server::export{ ['/etc/slurm', '/mnt/home', '/mnt/project', '/mnt/scratch'] :
     ensure  => 'mounted',
     clients => "$cidr(rw,sync,no_root_squash,no_all_squash)"
   }
