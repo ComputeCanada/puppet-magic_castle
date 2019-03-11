4c16e4d4d782ba7e6c6e7c1481270e6b592a33b1
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-03-11 17:38:20 -0400

    Simplify ipa_create_user script
    Also allow reusing a previous volume

--- site/profile/files/freeipa/ipa_create_user.sh
+++ site/profile/files/freeipa/ipa_create_user.sh
@@ -26,49 +26,48 @@ if [ -z "${IPA_GUEST_PASSWD+xxx}" ]; then
     IPA_GUEST_PASSWD="$GUEST_PASSWORD_INPUT"
 fi
 
+echo $IPA_ADMIN_PASSWD | kinit admin
+
+if [ -n "${SPONSOR}" ]; then
+    GROUP="def-$SPONSOR"
+    if ! ipa group-find --group-name="$GROUP" &> /dev/null; then
+        GID=$(ipa group-add "$GROUP" | grep -oP '(?<=GID: )[0-9]*')
+        mkdir -p "/project/$GID"
+        chown root:"$GROUP" "/project/$GID"
+        chmod 770 "/project/$GID"
+        ln -sfT "/project/$GID" "/project/$GROUP"
+    fi
+fi
+
 for USERNAME in ${USERNAMES[@]}; do
-    USER_HOME="/mnt/home/$USERNAME"
-    # Skip if the username has already been created
-    if [[ -d "$USER_HOME" ]] ; then
-        continue
-    fi
+    if ! ipa user-find --login=$USERNAME &> /dev/null; then
+        echo $IPA_GUEST_PASSWD | ipa user-add $USERNAME --first "-" --last "-" --cn "$USERNAME" --shell /bin/bash --password
+        echo -e "$IPA_GUEST_PASSWD\n$IPA_GUEST_PASSWD\n$IPA_GUEST_PASSWD" | kinit $USERNAME && kdestroy
 
-    echo $IPA_ADMIN_PASSWD | kinit admin
-    echo $IPA_GUEST_PASSWD | ipa user-add $USERNAME --first "-" --last "-" --cn "$USERNAME" --shell /bin/bash --password
-    kdestroy
-    echo -e "$IPA_GUEST_PASSWD\n$IPA_GUEST_PASSWD\n$IPA_GUEST_PASSWD" | kinit $USERNAME
-    kdestroy
-
-    cp -r /etc/skel $USER_HOME
-    chown -R $USERNAME:$USERNAME $USER_HOME
-
-    # Project space
-    if [ -n "${SPONSOR}" ]; then
-        echo $IPA_ADMIN_PASSWD | kinit admin
-        GROUP="def-$SPONSOR"
-        if ! ipa group-find "$GROUP" ; then
-            GID=$(ipa group-add "$GROUP" | grep -oP '(?<=GID: )[0-9]*')
-            mkdir -p "/project/$GID"
-            chown root:"$GROUP" "/project/$GID"
-            chmod 770 "/project/$GID"
-            ln -sfT "/project/$GID" "/project/$GROUP"
+        USER_HOME="/mnt/home/$USERNAME"
+        if [[ ! -d "$USER_HOME" ]] ; then
+            cp -r /etc/skel $USER_HOME
         fi
-        ipa group-add-member "$GROUP" --user="$USERNAME"
-        kdestroy
+        chown -R $USERNAME:$USERNAME $USER_HOME
 
-        PRO_USER="/project/$GROUP/$USERNAME"
-        mkdir -p $PRO_USER
-        mkdir -p "$USER_HOME/projects"
-        ln -sfT "/project/$GROUP" "$USER_HOME/projects/$GROUP"
-        ln -sfT "/project/$GROUP" "$USER_HOME/project"
-        chown -R $USERNAME:$USERNAME "$USER_HOME/projects" "$USER_HOME/project" $PRO_USER
-        chmod 750 "$USER_HOME/projects" $PRO_USER
+        # Project space
+        if [ -n "${GROUP}" ]; then
+            ipa group-add-member "$GROUP" --user="$USERNAME"
+            PRO_USER="/project/$GROUP/$USERNAME"
+            mkdir -p $PRO_USER
+            mkdir -p "$USER_HOME/projects"
+            ln -sfT "/project/$GROUP" "$USER_HOME/projects/$GROUP"
+            ln -sfT "/project/$GROUP" "$USER_HOME/project"
+            chown -R $USERNAME:$USERNAME "$USER_HOME/projects" "$USER_HOME/project" $PRO_USER
+            chmod 750 "$USER_HOME/projects" $PRO_USER
+        fi
+
+        # Scratch spaces
+        SCR_USER="/scratch/$USERNAME"
+        mkdir -p $SCR_USER
+        ln -sfT $SCR_USER "$USER_HOME/scratch"
+        chown -h $USERNAME:$USERNAME $SCR_USER "$USER_HOME/scratch"
+        chmod 750 $SCR_USER
     fi
-
-    # Scratch spaces
-    SCR_USER="/scratch/$USERNAME"
-    mkdir -p $SCR_USER
-    ln -sfT $SCR_USER "$USER_HOME/scratch"
-    chown -h $USERNAME:$USERNAME $SCR_USER "$USER_HOME/scratch"
-    chmod 750 $SCR_USER
 done
+kdestroy -A
