3e364cdc1f0425327c2647b04f5826d98878b61d
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-04-06 16:26:19 -0400

    Move /projects pictures to workshop class

---
+++ manifests/workshop.pp
@@ -0,0 +1,15 @@
+class profile::workshop {
+  file { ['/project/6002799', '/project/6002799/photos'] :
+    ensure => directory
+  }
+
+  file { '/project/6002799/photos/KSC2018.jpg':
+    ensure => 'present',
+    source => "https://images-assets.nasa.gov/image/KSC-20180316-PH_JBS01_0118/KSC-20180316-PH_JBS01_0118~orig.JPG"
+  }
+
+  file { "/project/6002799/photos/VAFB2018.jpg":
+    ensure => 'present',
+    source => "https://images-assets.nasa.gov/image/VAFB-20180302-PH_ANV01_0056/VAFB-20180302-PH_ANV01_0056~orig.jpg"
+  }  
+}
