35b727ed29a4fdeb8207400d141c0de38b1fb9cb
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-04-19 12:29:36 -0400

    Make Bye Bye optional in fail2ban filter

--- manifests/fail2ban.pp
+++ manifests/fail2ban.pp
@@ -29,7 +29,7 @@ class profile::fail2ban {
   file_line { 'fail2ban_sshd_recv_disconnect':
     ensure => present,
     path   => '/etc/fail2ban/filter.d/sshd.conf',
-    line   => '         ^%(__prefix_line_sl)sReceived disconnect from <HOST>%(__on_port_opt)s:\s*11: Bye Bye%(__suff)s$',
+    line   => '         ^%(__prefix_line_sl)sReceived disconnect from <HOST>%(__on_port_opt)s:\s*11:( Bye Bye)?%(__suff)s$',
     after  => '^normal\ \=*',
     notify => Service['fail2ban']
   }
