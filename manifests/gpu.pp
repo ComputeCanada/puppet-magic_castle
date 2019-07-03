1b27afcc424e120dc27d4313c76af5f56d927968
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-07-03 10:14:21 -0400

    Update gpu module

--- manifests/gpu.pp
+++ manifests/gpu.pp
@@ -1,25 +1,23 @@
 class profile::gpu {
-  file_line { 'kmod_nvidia_exclude':
-    ensure => present,
-    path   => '/etc/yum.conf',
-    line   => 'exclude=nvidia-x11*',
-  }
-
-  class { 'yum':
-    config_options => {
-        obsoletes => false
-      },
-    }
-
   package { 'cuda-repo':
     ensure   => 'installed',
     provider => 'rpm',
     name     => 'cuda-repo-rhel7',
-    source   => 'http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm'
+    source   => 'http://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.1.168-1.x86_64.rpm'
   }
 
-  package { ['nvidia-kmod-396.26', 'xorg-x11-drv-nvidia-396.26', 'xorg-x11-drv-nvidia-devel-396.26', 'cuda-drivers-396.26']:
+  package { [
+    'nvidia-driver-cuda-libs',
+    'nvidia-driver-NVML',
+    'nvidia-driver-NvFBCOpenGL',
+    'nvidia-driver-libs',
+    'dkms-nvidia',
+    'nvidia-driver',
+    'nvidia-driver-cuda',
+    'nvidia-driver-devel',
+    'nvidia-modprobe',
+    ]:
     ensure  => 'installed',
-    require => [Class['yum'], File_line['kmod_nvidia_exclude'], Package['cuda-repo']]
+    require => Package['cuda-repo']
   }
 }
