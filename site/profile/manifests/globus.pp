b370aeba01a1fa47becbaddffa96583b60505e11
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-08-28 09:36:27 -0400

    Make globus rpm install and config file if globus_user/pass only

--- site/profile/manifests/globus.pp
+++ site/profile/manifests/globus.pp
@@ -7,21 +7,21 @@ class profile::globus::base (String $globus_user = '', String $globus_password =
     source   => 'https://downloads.globus.org/toolkit/globus-connect-server/globus-connect-server-repo-latest.noarch.rpm'
   }
 
-  package { 'globus-connect-server':
-    ensure  => 'installed',
-    require => [Package['yum-plugin-priorities'],
-                Package['globus-connect-server-repo']]
-  }
-
   $domain_name = lookup('profile::freeipa::base::domain_name')
   $cluster_name = split($domain_name, '.')[0]
-  file { '/etc/globus-connect-server.conf':
-    ensure  => 'present',
-    content => epp('profile/globus/globus-connect-server.conf', { 'domain_name' => $domain_name,
-                                                                  'hostname'    => $cluster_name }),
-  }
 
   if ($globus_user != '') and ($globus_password != '') {
+    package { 'globus-connect-server':
+      ensure  => 'installed',
+      require => [Package['yum-plugin-priorities'],
+                  Package['globus-connect-server-repo']]
+    }
+
+    file { '/etc/globus-connect-server.conf':
+      ensure  => 'present',
+      content => epp('profile/globus/globus-connect-server.conf', { 'domain_name' => $domain_name,
+                                                                    'hostname'    => $cluster_name }),
+    }
 
     firewall { '100 Globus connect server - globus.org':
       chain  => 'INPUT',
