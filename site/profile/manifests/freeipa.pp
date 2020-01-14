54947a796fee1b7a3e03e81320eb1c37f4c8870a
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-01-14 09:06:58 -0500

    Replace eth0 by interface index 0 in freeipa ptr record

--- site/profile/manifests/freeipa.pp
+++ site/profile/manifests/freeipa.pp
@@ -146,10 +146,12 @@ class profile::freeipa::client(String $server_ip)
 
   $reverse_zone = profile::getreversezone()
   $ptr_record = profile::getptrrecord()
+  $interface = split($::interfaces, ',')[0]
+  $ipaddress = $::networking['interfaces'][$interface]['ip']
 
   exec { 'ipa_dnsrecord-del_ptr':
     command     => "kinit_wrapper ipa dnsrecord-del ${reverse_zone} ${ptr_record} --del-all",
-    onlyif      => "test `dig -x ${::ipaddress_eth0} | grep -oP '^.*\\s[0-9]*\\sIN\\sPTR\\s\\K(.*)'` != ${fqdn}.",
+    onlyif      => "test `dig -x ${ipaddress} | grep -oP '^.*\\s[0-9]*\\sIN\\sPTR\\s\\K(.*)'` != ${fqdn}.",
     require     => [File['kinit_wrapper'], Exec['ipa-client-install']],
     environment => ["IPA_ADMIN_PASSWD=${admin_passwd}"],
     path        => ['/bin', '/usr/bin', '/sbin','/usr/sbin']
@@ -157,7 +159,7 @@ class profile::freeipa::client(String $server_ip)
 
   exec { 'ipa_dnsrecord-add_ptr':
     command     => "kinit_wrapper ipa dnsrecord-add ${reverse_zone} ${ptr_record} --ptr-hostname=${fqdn}.",
-    unless      => "dig -x ${::ipaddress_eth0} | grep -q ';; ANSWER SECTION:'",
+    unless      => "dig -x ${ipaddress} | grep -q ';; ANSWER SECTION:'",
     require     => [File['kinit_wrapper'], Exec['ipa-client-install'], Exec['ipa_dnsrecord-del_ptr']],
     environment => ["IPA_ADMIN_PASSWD=${admin_passwd}"],
     path        => ['/bin', '/usr/bin', '/sbin','/usr/sbin']
