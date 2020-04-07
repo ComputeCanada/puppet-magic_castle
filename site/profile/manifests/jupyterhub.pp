606ca1787f794f04a50ce64047802a3e00037ae4
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-04-07 16:23:27 -0400

    Define dependency in jupyterhub::node if setup is venv

--- site/profile/manifests/jupyterhub.pp
+++ site/profile/manifests/jupyterhub.pp
@@ -4,5 +4,7 @@ class profile::jupyterhub::hub {
 
 class profile::jupyterhub::node {
   include jupyterhub::node
-  Class['profile::cvmfs::client'] -> Class['jupyterhub::kernel::venv']
+  if lookup('jupyterhub::kernel::setup') == 'venv' {
+    Class['profile::cvmfs::client'] -> Class['jupyterhub::kernel::venv']
+  }
 }
