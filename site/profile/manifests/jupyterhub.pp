803c09103b825cb956e70494f53383e22e404c13
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-04-08 00:20:21 -0400

    Move class kernel dependency in node prefix not cvmfs

--- site/profile/manifests/jupyterhub.pp
+++ site/profile/manifests/jupyterhub.pp
@@ -5,8 +5,8 @@ class profile::jupyterhub::hub {
 class profile::jupyterhub::node {
   if lookup('jupyterhub::node::prefix') !~ /^\/cvmfs.*/ {
     include jupyterhub::node
-  }
-  if lookup('jupyterhub::kernel::setup') == 'venv' {
-    Class['profile::cvmfs::client'] -> Class['jupyterhub::kernel::venv']
+    if lookup('jupyterhub::kernel::setup') == 'venv' {
+      Class['profile::cvmfs::client'] -> Class['jupyterhub::kernel::venv']
+    }
   }
 }
