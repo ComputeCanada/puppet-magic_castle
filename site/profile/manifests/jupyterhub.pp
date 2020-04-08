acaa25e36b050993ef2ea871f498191b64300c25
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-04-08 00:19:08 -0400

    Add condition to include jupyterhub::node

--- site/profile/manifests/jupyterhub.pp
+++ site/profile/manifests/jupyterhub.pp
@@ -3,7 +3,9 @@ class profile::jupyterhub::hub {
 }
 
 class profile::jupyterhub::node {
-  include jupyterhub::node
+  if lookup('jupyterhub::node::prefix') !~ /^\/cvmfs.*/ {
+    include jupyterhub::node
+  }
   if lookup('jupyterhub::kernel::setup') == 'venv' {
     Class['profile::cvmfs::client'] -> Class['jupyterhub::kernel::venv']
   }
