89bec2774ae26e97b1da527cf2cc67d7832ab1cc
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-05-22 13:42:15 -0400

    Add class profile::gpu::install

--- site/profile/manifests/gpu.pp
+++ site/profile/manifests/gpu.pp
@@ -1,6 +1,69 @@
-class profile::gpu (Array[String] $packages) {
+class profile::gpu {
+  require profile::gpu::install
+
+  if $facts['nvidia_gpu_count'] > 0 {
+    if ! $facts['nvidia_grid_vgpu'] {
+      file { '/var/run/nvidia-persistenced':
+        ensure => directory,
+        owner  => 'nvidia-persistenced',
+        group  => 'nvidia-persistenced',
+        mode   => '0755',
+      }
+
+      augeas { 'nvidia-persistenced.service':
+        context => '/files/lib/systemd/system/nvidia-persistenced.service/Service',
+        changes => [
+          'set User/value nvidia-persistenced',
+          'set Group/value nvidia-persistenced',
+          'rm ExecStart/arguments',
+        ],
+      }
+
+      service { 'nvidia-persistenced':
+        ensure  => 'running',
+        enable  => true,
+        require => [
+          File['/var/run/nvidia-persistenced'],
+          Augeas['nvidia-persistenced.service'],
+        ],
+      }
+    }
+  }
+
+  file { '/usr/lib64/nvidia':
+    ensure => directory
+  }
 
   $driver_ver = $::facts['nvidia_driver_version']
+  $nvidia_libs = [
+    "libnvidia-ml.so.${driver_ver}", 'libnvidia-ml.so.1', 'libnvidia-fbc.so.1',
+    "libnvidia-fbc.so.${driver_ver}", 'libnvidia-ifr.so.1', "libnvidia-ifr.so.${driver_ver}",
+    'libcuda.so', 'libcuda.so.1', "libcuda.so.${driver_ver}", "libnvcuvid.so.${driver_ver}",
+    'libnvcuvid.so.1', "libnvidia-compiler.so.${driver_ver}", 'libnvidia-encode.so.1',
+    "libnvidia-encode.so.${driver_ver}", "libnvidia-fatbinaryloader.so.${driver_ver}",
+    'libnvidia-opencl.so.1', "libnvidia-opencl.so.${driver_ver}", 'libnvidia-opticalflow.so.1',
+    "libnvidia-opticalflow.so.${driver_ver}", 'libnvidia-ptxjitcompiler.so.1', "libnvidia-ptxjitcompiler.so.${driver_ver}",
+    'libnvcuvid.so', 'libnvidia-cfg.so', 'libnvidia-encode.so',
+    'libnvidia-fbc.so', 'libnvidia-ifr.so', 'libnvidia-ml.so',
+    'libnvidia-ptxjitcompiler.so', 'libEGL_nvidia.so.0', "libEGL_nvidia.so.${driver_ver}",
+    'libGLESv1_CM_nvidia.so.1', "libGLESv1_CM_nvidia.so.${driver_ver}", 'libGLESv2_nvidia.so.2',
+    "libGLESv2_nvidia.so.${driver_ver}", 'libGLX_indirect.so.0', 'libGLX_nvidia.so.0',
+    "libGLX_nvidia.so.${driver_ver}", "libnvidia-cbl.so.${driver_ver}", 'libnvidia-cfg.so.1',
+    "libnvidia-cfg.so.${driver_ver}", "libnvidia-eglcore.so.${driver_ver}", "libnvidia-glcore.so.${driver_ver}",
+    "libnvidia-glsi.so.${driver_ver}", "libnvidia-glvkspirv.so.${driver_ver}", "libnvidia-rtcore.so.${driver_ver}",
+    "libnvidia-tls.so.${driver_ver}", 'libnvoptix.so.1', "libnvoptix.so.${driver_ver}"]
+
+  $nvidia_libs.each |String $lib| {
+    file { "/usr/lib64/nvidia/${lib}":
+      ensure  => link,
+      target  => "/usr/lib64/${lib}",
+      seltype => 'lib_t'
+    }
+  }
+
+}
+
+class profile::gpu::install(Array[String] $packages) {
   if ! $facts['nvidia_grid_vgpu'] {
     $cuda_ver = $::facts['nvidia_cuda_version']
     $os = "rhel${::facts['os']['release']['major']}"
@@ -48,63 +111,5 @@ class profile::gpu (Array[String] $packages) {
       ]:
       require => Exec['dkms autoinstall']
     }
-
-    if ! $facts['nvidia_grid_vgpu'] {
-      file { '/var/run/nvidia-persistenced':
-        ensure => directory,
-        owner  => 'nvidia-persistenced',
-        group  => 'nvidia-persistenced',
-        mode   => '0755',
-      }
-
-      augeas { 'nvidia-persistenced.service':
-        context => '/files/lib/systemd/system/nvidia-persistenced.service/Service',
-        changes => [
-          'set User/value nvidia-persistenced',
-          'set Group/value nvidia-persistenced',
-          'rm ExecStart/arguments',
-        ],
-      }
-
-      service { 'nvidia-persistenced':
-        ensure  => 'running',
-        enable  => true,
-        require => [
-          File['/var/run/nvidia-persistenced'],
-          Augeas['nvidia-persistenced.service'],
-        ],
-      }
-    }
   }
-
-  file { '/usr/lib64/nvidia':
-    ensure => directory
-  }
-
-  $nvidia_libs = [
-    "libnvidia-ml.so.${driver_ver}", 'libnvidia-ml.so.1', 'libnvidia-fbc.so.1',
-    "libnvidia-fbc.so.${driver_ver}", 'libnvidia-ifr.so.1', "libnvidia-ifr.so.${driver_ver}",
-    'libcuda.so', 'libcuda.so.1', "libcuda.so.${driver_ver}", "libnvcuvid.so.${driver_ver}",
-    'libnvcuvid.so.1', "libnvidia-compiler.so.${driver_ver}", 'libnvidia-encode.so.1',
-    "libnvidia-encode.so.${driver_ver}", "libnvidia-fatbinaryloader.so.${driver_ver}",
-    'libnvidia-opencl.so.1', "libnvidia-opencl.so.${driver_ver}", 'libnvidia-opticalflow.so.1',
-    "libnvidia-opticalflow.so.${driver_ver}", 'libnvidia-ptxjitcompiler.so.1', "libnvidia-ptxjitcompiler.so.${driver_ver}",
-    'libnvcuvid.so', 'libnvidia-cfg.so', 'libnvidia-encode.so',
-    'libnvidia-fbc.so', 'libnvidia-ifr.so', 'libnvidia-ml.so',
-    'libnvidia-ptxjitcompiler.so', 'libEGL_nvidia.so.0', "libEGL_nvidia.so.${driver_ver}",
-    'libGLESv1_CM_nvidia.so.1', "libGLESv1_CM_nvidia.so.${driver_ver}", 'libGLESv2_nvidia.so.2',
-    "libGLESv2_nvidia.so.${driver_ver}", 'libGLX_indirect.so.0', 'libGLX_nvidia.so.0',
-    "libGLX_nvidia.so.${driver_ver}", "libnvidia-cbl.so.${driver_ver}", 'libnvidia-cfg.so.1',
-    "libnvidia-cfg.so.${driver_ver}", "libnvidia-eglcore.so.${driver_ver}", "libnvidia-glcore.so.${driver_ver}",
-    "libnvidia-glsi.so.${driver_ver}", "libnvidia-glvkspirv.so.${driver_ver}", "libnvidia-rtcore.so.${driver_ver}",
-    "libnvidia-tls.so.${driver_ver}", 'libnvoptix.so.1', "libnvoptix.so.${driver_ver}"]
-
-  $nvidia_libs.each |String $lib| {
-    file { "/usr/lib64/nvidia/${lib}":
-      ensure  => link,
-      target  => "/usr/lib64/${lib}",
-      seltype => 'lib_t'
-    }
-  }
-
 }
