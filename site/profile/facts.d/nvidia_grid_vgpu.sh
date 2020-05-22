424658327b091e6dc87d05929de5b712116aa002
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-05-22 17:32:33 -0400

    Update nvidia_grid_vgpu fact to use metadata server

--- site/profile/facts.d/nvidia_grid_vgpu.sh
+++ site/profile/facts.d/nvidia_grid_vgpu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo "{ 'nvidia_grid_vgpu' : $(test -f /etc/nvidia/gridd.conf && echo true || echo false) }"
+echo "{ 'nvidia_grid_vgpu' : $(curl -s http://169.254.169.254/2009-04-04/meta-data/instance-type | grep -q ^vgpu && echo true || echo false) }"
