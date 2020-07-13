77a6420791219a609d95682a0292ccfcaa68fd86
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-07-13 16:07:24 -0400

    Add health check on squid free memory

--- site/profile/manifests/squid.pp
+++ site/profile/manifests/squid.pp
@@ -20,4 +20,16 @@ class profile::squid::server (Integer $port = 3128) {
     require => Tcp_conn_validator['consul'],
     token   => lookup('profile::consul::acl_api_token'),
   }
+
+  consul::check { 'squid_free_mem':
+    interval   => '30s',
+    service_id => 'squid',
+    args       => [
+      '/bin/sh',
+      '-c',
+      '/usr/bin/free | awk \'/Mem/{printf($3/$2*100)}\' | awk \'{ print($0); if($1 > 70) exit 1;}\''
+    ],
+    token      => lookup('profile::consul::acl_api_token'),
+  }
+
 }
