c7b80a54cc2192d61a351ac5313a81427ec9b85c
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-10-28 15:42:16 -0400

    Give a memory to mkprojectdaemon.sh

--- site/profile/files/freeipa/mkprojectdaemon.sh
+++ site/profile/files/freeipa/mkprojectdaemon.sh
@@ -1,24 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 BASEDN="$(grep -oP 'basedn = \K(.*)' /etc/ipa/default.conf)"
+
+declare -A group_memory
 
 tail -f /var/log/dirsrv/slapd-*/access |
 grep --line-buffered -oP 'MOD dn=\"cn=\K(def-[a-z0-9A-Z_-]*)(?=,cn=groups)' |
 while read GROUP; do
-    kinit -k -t /etc/mokey/keytab/mokeyapp.keytab mokeyapp
-    USERNAME=$(ldapsearch -Q -s one -b "cn=users,cn=accounts,${BASEDN}" "memberOf=*${GROUP}*" uid | grep -oP 'uid: \K(.*)' | tail -n1)
-    kdestroy
 
-    if [[ ! -d /project/$GROUP ]]; then
-        GID=$(getent group $GROUP | cut -d: -f3)
-        mkdir -p "/project/$GID"
-        chown root:"$GROUP" "/project/$GID"
-        chmod 2770 "/project/$GID"
-        ln -sfT "/project/$GID" "/project/$GROUP"
-        restorecon -F -R /project/$GROUP
+    if [[ -z ${group_memory[$GROUP]} ]]; then
+        group_memory[$GROUP]=1
+        if [[ ! -d /project/$GROUP ]]; then
+            GID=$(getent group $GROUP | cut -d: -f3)
+            mkdir -p "/project/$GID"
+            chown root:"$GROUP" "/project/$GID"
+            chmod 2770 "/project/$GID"
+            ln -sfT "/project/$GID" "/project/$GROUP"
+            restorecon -F -R /project/$GROUP
+        fi
     fi
 
-    if [[ ! -z "${USERNAME}" ]]; then
+    kinit -k -t /etc/mokey/keytab/mokeyapp.keytab mokeyapp
+    USERNAMES=$(ldapsearch -Q -s one -b "cn=users,cn=accounts,${BASEDN}" "memberOf=*${GROUP}*" uid | grep -oP 'uid: \K(.*)' | tail -n +${group_memory[$GROUP]})
+    kdestroy
+
+    for USERNAME in $USERNAME; do
         USER_HOME="/mnt/home/$USERNAME"
 
         PRO_USER="/project/$GROUP/$USERNAME"
@@ -31,6 +37,7 @@ while read GROUP; do
         chmod 0755 "$USER_HOME/projects"
         chmod 2700 $PRO_USER
         restorecon -F -R /project/$GROUP/$USERNAME
-    fi
+        group_memory[$GROUP]=$((${group_memory[$GROUP]}+1))
+    done
 
 done
