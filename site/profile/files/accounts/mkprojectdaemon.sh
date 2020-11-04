1534d8f7e2792c9cf1ddb47768ec8a169111220a
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-11-03 23:17:30 -0500

    Add count of calls in mkprojectdaemon.sh

--- site/profile/files/accounts/mkprojectdaemon.sh
+++ site/profile/files/accounts/mkprojectdaemon.sh
@@ -3,16 +3,22 @@
 BASEDN="$(grep -oP 'basedn = \K(.*)' /etc/ipa/default.conf)"
 export KRB5CCNAME="/root/mkprojectdaemon.krb5"
 
-declare -A group_memory
+declare -A group_memory_calls
+declare -A group_memory_users
 
 PROJECT_PREFIX="ctb|def|rpp|rrg"
 
 tail -f /var/log/dirsrv/slapd-*/access |
 grep --line-buffered -oP "MOD dn=\"cn=\K((${PROJECT_PREFIX})-[a-z0-9A-Z_-]*)(?=,cn=groups)" |
 while read GROUP; do
+    if [[ -z ${group_memory_calls[$GROUP]} ]]; then
+        group_memory_calls[$GROUP]=1
+    else
+        group_memory_calls[$GROUP]=$((${group_memory_calls[$GROUP]}+1))
+    fi
 
-    if [[ -z ${group_memory[$GROUP]} ]]; then
-        group_memory[$GROUP]=1
+    if [[ -z ${group_memory_users[$GROUP]} ]]; then
+        group_memory_users[$GROUP]=0
         /opt/software/slurm/bin/sacctmgr add account $GROUP -i Description='Cloud Cluster Account' Organization='Compute Canada'
         if [[ ! -d /project/$GROUP ]]; then
             GID=$(getent group $GROUP | cut -d: -f3)
@@ -24,8 +30,13 @@ while read GROUP; do
         fi
     fi
 
+    # Skip ldapsearch if we have already processed more usernames than the number of lines found so far in log
+    if [[ ${group_memory_users[$GROUP]} -geq ${group_memory_calls[$GROUP]} ]];
+        continue
+    then
+
     kinit -k -t /etc/mokey/keytab/mokeyapp.keytab mokeyapp
-    USERNAMES=$(ldapsearch -Q -s one -b "cn=users,cn=accounts,${BASEDN}" "memberOf=*${GROUP}*" uid | grep -oP 'uid: \K(.*)' | tail -n +${group_memory[$GROUP]})
+    USERNAMES=$(ldapsearch -Q -s one -b "cn=users,cn=accounts,${BASEDN}" "memberOf=*${GROUP}*" uid | grep -oP 'uid: \K(.*)' | tail -n +$((${group_memory_users[$GROUP]}+1)))
     kdestroy
 
     if [[ -z "$USERNAMES" ]]; then
@@ -45,7 +56,7 @@ while read GROUP; do
         chmod 0755 "$USER_HOME/projects"
         chmod 2700 $PRO_USER
         restorecon -F -R /project/$GROUP/$USERNAME
-        group_memory[$GROUP]=$((${group_memory[$GROUP]}+1))
+        group_memory_users[$GROUP]=$((${group_memory_users[$GROUP]}+1))
     done
 
     /opt/software/slurm/bin/sacctmgr add user ${USERNAMES[@]} Account=${GROUP} -i
