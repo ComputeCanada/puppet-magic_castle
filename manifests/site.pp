b63fec047efb8416bfc72230980a45234b41aa69
Darren Boss <darren.boss@computecanada.ca>
2020-11-04 19:10:42 -0500

    Correct duo module name

--- manifests/site.pp
+++ manifests/site.pp
@@ -40,7 +40,7 @@ node /^mgmt1$/ {
   include profile::freeipa::guest_accounts
   include profile::slurm::accounting
   include profile::workshop::mgmt
-  include iu-duounix
+  include iu-duo_unix
 
 node /^mgmt(?:[2-9]|[1-9]\d\d*)$/ {
   include profile::consul::client
@@ -49,6 +49,7 @@ node /^mgmt(?:[2-9]|[1-9]\d\d*)$/ {
   include profile::rsyslog::client
   include profile::freeipa::client
   include profile::metrics::exporter
+  include iu-duo_unix
 }
 
 node /^[a-z0-9-]*node\d+$/ {
@@ -64,4 +65,5 @@ node /^[a-z0-9-]*node\d+$/ {
   include profile::nfs::client
   include profile::slurm::node
   include profile::freeipa::client
+  include iu-duo_unix
 }
