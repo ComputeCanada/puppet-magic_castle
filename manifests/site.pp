be3cf96eb801ffc5bc290b786a934dee871e7b1b
Darren Boss <darren.boss@computecanada.ca>
2020-11-09 12:42:21 -0500

    First attempt at conditionals for mfa

--- manifests/site.pp
+++ manifests/site.pp
@@ -23,6 +23,9 @@ node /^login\d+$/ {
   include profile::reverse_proxy
   include profile::nfs::client
   include profile::freeipa::client
+  if $duo_unix::login {
+    include duo_unix
+  }
 }
 
 node /^mgmt1$/ {
@@ -40,7 +43,9 @@ node /^mgmt1$/ {
   include profile::freeipa::guest_accounts
   include profile::slurm::accounting
   include profile::workshop::mgmt
-  include duo_unix
+  if $duo_unix::mgmt {
+    include duo_unix
+  }
 }
 
 node /^mgmt(?:[2-9]|[1-9]\d\d*)$/ {
@@ -50,7 +55,9 @@ node /^mgmt(?:[2-9]|[1-9]\d\d*)$/ {
   include profile::rsyslog::client
   include profile::freeipa::client
   include profile::metrics::exporter
-  include duo_unix
+  if $duo_unix::mgmt {
+    include duo_unix
+  }
 }
 
 node /^[a-z0-9-]*node\d+$/ {
@@ -66,5 +73,8 @@ node /^[a-z0-9-]*node\d+$/ {
   include profile::nfs::client
   include profile::slurm::node
   include profile::freeipa::client
-  include duo_unix
+  if $duo_unix::node {
+    include duo_unix
+  }
+
 }
