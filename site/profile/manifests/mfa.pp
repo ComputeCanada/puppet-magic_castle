626949fb9133b0570c944db7ef75071327964a25
Darren Boss <darren.boss@computecanada.ca>
2020-11-09 21:59:27 -0500

    Need a lookup to read from user data

--- site/profile/manifests/mfa.pp
+++ site/profile/manifests/mfa.pp
@@ -1,19 +1,19 @@
 # MFA class which allows for multiple MFA implementations to
 # be deployed in one cluster deployment.
 class profile::mfa::duo::login {
-  if $profile::mfa::duo::login {
+  if lookup(profile::mfa::duo::login) {
     include duo_unix
   }
 }
 
 class profile::mfa::duo::mgmt {
-  if $profile::mfa::duo::mgmt {
+  if lookup(profile::mfa::duo::mgmt) {
     include duo_unix
   }
 }
 
 class profile::mfa::duo::node {
-  if $profile::mfa::duo::node {
+  if lookup(profile::mfa::duo::node) {
     include duo_unix
   }
 }
