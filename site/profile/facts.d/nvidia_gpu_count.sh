af7a5bb3f8497c5a24c5723ce89ce6e3c605bff5
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-11-13 13:07:51 -0500

    Update nvidia_grid_vgpu logic to compare available memory

--- site/profile/facts.d/nvidia_gpu_count.sh
+++ site/profile/facts.d/nvidia_gpu_count.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "{ 'nvidia_gpu_count' : $(lspci | grep -c -i nvidia) }"
+# NVIDIA vendor id is 0x10de
+# List all devides with that vendor id then count the number of lines
+echo "{ 'nvidia_gpu_count' : $(lspci -d 0x10de: | wc -l) }"
--- site/profile/facts.d/nvidia_grid_vgpu.sh
+++ site/profile/facts.d/nvidia_grid_vgpu.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-echo "{ 'nvidia_grid_vgpu' : $(curl -s http://169.254.169.254/2009-04-04/meta-data/instance-type | grep -q ^vgpu && echo true || echo false) }"
+
+# NVIDIA Vendor ID is : 0x10de
+# Grep the GPU memory size from the product description
+GPU_MEM_SIZE=$(/usr/sbin/lspci -d 10de: | grep -oP '\[*\K[0-9]*[T|G](?=B)')
+# Grep the actual GPU memory size from lspci verbose
+AVAIL_MEM_SIZE=$(/usr/sbin/lspci -d 10de: -v | grep -oP 'Memory.*\(64-bit, prefetchable\).*\[size=\K([0-9]*[T|G])(?=])')
+
+# If the memory available from the product description is different than the available memory
+# we conclude it must be a virtual GPU
+if [[ ! -z "${GPU_MEM_SIZE}" ]] && [[ ! -z "${AVAIL_MEM_SIZE}" ]]; then
+    IS_VGPU=$(test "${GPU_MEM_SIZE}" != "${AVAIL_MEM_SIZE}" && echo true || echo false)
+else
+    IS_VGPU='false'
+fi
+
+echo "{ 'nvidia_grid_vgpu' : $IS_VGPU }"
