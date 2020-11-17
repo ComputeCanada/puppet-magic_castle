e369d11af97fa06dcdf81dc3c59c05cd485f12a2
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-11-17 11:10:21 -0500

    Disable just links instead of whole template when signup is false

--- site/profile/manifests/jupyterhub.pp
+++ site/profile/manifests/jupyterhub.pp
@@ -3,21 +3,15 @@ class profile::jupyterhub::hub {
   Yumrepo['epel'] -> Class['jupyterhub']
 
   $enable_user_signup = lookup('profile::freeipa::mokey::enable_user_signup')
-  if $enable_user_signup {
-    $ensure_login_template = 'present'
-  }
-  else {
-    $ensure_login_template = 'absent'
-  }
-
   $domain_name = lookup('profile::freeipa::base::domain_name')
   $mokey_subdomain = lookup('profile::reverse_proxy::mokey_subdomain')
   $mokey_hostname = "${mokey_subdomain}.${domain_name}"
 
   file { '/etc/jupyterhub/templates/login.html':
-    ensure  => $ensure_login_template,
+    ensure  => present,
     content => epp('profile/jupyterhub/login.html', {
-        'mokey_hostname' => $mokey_hostname,
+        'mokey_hostname'     => $mokey_hostname,
+        'enable_user_signup' => $enable_user_signup,
       }
     ),
   }
