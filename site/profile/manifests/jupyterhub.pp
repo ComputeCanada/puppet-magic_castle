bd822f74facde2134c5eb087645e4f4eadc74a61
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-11-17 09:34:59 -0500

    Disable jupyterhub login template if mokey signup is off

--- site/profile/manifests/jupyterhub.pp
+++ site/profile/manifests/jupyterhub.pp
@@ -2,12 +2,20 @@ class profile::jupyterhub::hub {
   contain jupyterhub
   Yumrepo['epel'] -> Class['jupyterhub']
 
+  $enable_user_signup = lookup('profile::freeipa::mokey::enable_user_signup')
+  if $enable_user_signup {
+    $ensure_login_template = 'present'
+  }
+  else {
+    $ensure_login_template = 'absent'
+  }
+
   $domain_name = lookup('profile::freeipa::base::domain_name')
   $mokey_subdomain = lookup('profile::reverse_proxy::mokey_subdomain')
   $mokey_hostname = "${mokey_subdomain}.${domain_name}"
 
   file { '/etc/jupyterhub/templates/login.html':
-    ensure  => present,
+    ensure  => $ensure_login_template,
     content => epp('profile/jupyterhub/login.html', {
         'mokey_hostname' => $mokey_hostname,
       }
