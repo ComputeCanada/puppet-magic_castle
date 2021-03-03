9027e3337756119eaabf1a6f5a9d4fe73620e3fd
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-03-03 09:27:43 -0500

    Complete simplification of mkproject

--- site/profile/files/accounts/mkproject.sh
+++ site/profile/files/accounts/mkproject.sh
@@ -1,48 +1,76 @@
 #!/bin/bash
 
-BASEDN="$(grep -oP 'basedn = \K(.*)' /etc/ipa/default.conf)"
-
-declare -A group_memory_users
-
 PROJECT_PREFIX="ctb|def|rpp|rrg"
+PREV_CONN=""
 
 tail -F /var/log/dirsrv/slapd-*/access |
-grep --line-buffered -P "MOD dn=\"cn=\K((${PROJECT_PREFIX})-[a-z0-9A-Z_-]*)(?=,cn=groups)" |
-while read LINE; do
-    GROUP=$(echo $LINE | grep -oP "MOD dn=\"cn=\K((${PROJECT_PREFIX})-[a-z0-9A-Z_-]*)(?=,cn=groups)")
-    USERNAME=$(grep -B 5 -F "$LINE" /var/log/dirsrv/slapd-*/access | grep -oP "SRCH base=\"uid=\K([a-z0-9A-Z_-]*)(?=,cn=users)")
-
-    if [[ -z ${group_memory_users[$GROUP]} ]]; then
-        group_memory_users[$GROUP]=0
-        /opt/software/slurm/bin/sacctmgr add account $GROUP -i Description='Cloud Cluster Account' Organization='Compute Canada'
-        if [[ ! -d /project/$GROUP ]]; then
-            GID=$(getent group $GROUP | cut -d: -f3)
-            mkdir -p "/project/$GID"
-            chown root:"$GROUP" "/project/$GID"
-            chmod 2770 "/project/$GID"
-            ln -sfT "/project/$GID" "/project/$GROUP"
-            restorecon -F -R /project/$GID /project/$GROUP
-        fi
-    fi
-
-    if [[ -z "$USERNAME" ]]; then
+grep --line-buffered -P "dn=\"cn=(${PROJECT_PREFIX})-[a-z0-9A-Z_-]*,cn=groups" |
+sed -u -r 's/^.*conn=([0-9]*) op=[0-9]* (\w+) dn="cn=(.*),cn=groups.*$/\1 \2 \3/' |
+while read CONN OP GROUP; do
+    # An operation has been done on a group in LDAP
+    # We have already completed this request
+    if [[ "$PREV_CONN" == "$CONN" ]]; then
         continue
     fi
 
-    USER_HOME="/mnt/home/$USERNAME"
+    # We wait for the operation $CONN to be completed
+    while ! grep -q "$CONN op=[0-9]* UNBIND" /var/log/dirsrv/slapd-*/access; do
+        sleep 1;
+    done
 
-    PRO_USER="/project/$GROUP/$USERNAME"
-    mkdir -p $PRO_USER
-    mkdir -p "$USER_HOME/projects"
-    ln -sfT "/project/$GROUP" "$USER_HOME/projects/$GROUP"
+    # We support three operations : ADD, MOD or DEL
+    if [[ "$OP" == "ADD" ]]; then
+        # A new group has been created
+        # We create the associated account in slurm
+        /opt/software/slurm/bin/sacctmgr add account $GROUP -i
 
-    chgrp $USERNAME "$USER_HOME/projects"
-    chown $USERNAME $PRO_USER
-    chmod 0755 "$USER_HOME/projects"
-    chmod 2700 $PRO_USER
-    restorecon -F -R /project/$GROUP/$USERNAME
-    group_memory_users[$GROUP]=$((${group_memory_users[$GROUP]}+1))
+        # Then we create the project folder
+        GID=$(getent group $GROUP | cut -d: -f3)
+        mkdir -p "/project/$GID"
+        chown root:"$GROUP" "/project/$GID"
+        chmod 2770 "/project/$GID"
+        ln -sfT "/project/$GID" "/project/$GROUP"
+        restorecon -F -R /project/$GID /project/$GROUP
 
-    /opt/software/slurm/bin/sacctmgr add user ${USERNAME} Account=${GROUP} -i
+    elif [[ "$OP" == "MOD" ]]; then
+        # A group has been modified
+        # We grep the log for all operations related to request $CONN that contain a uid
+        USERNAMES=$(grep -oP "conn=$CONN op=[0-9]* SRCH base=\"uid=\K([a-z0-9A-Z_-]*)(?=,cn=users)" /var/log/dirsrv/slapd-*/access)
 
+        # The operation that add users to a group would have requests with a uid
+        # If we found none, $USERNAMES will be empty, and it means we don't have
+        # anything to add to Slurm and /project
+        if [[ -z "$USERNAMES" ]]; then
+            continue
+        fi
+
+        for USERNAME in $USERNAMES; do
+            USER_HOME="/mnt/home/$USERNAME"
+
+            PRO_USER="/project/$GROUP/$USERNAME"
+            mkdir -p $PRO_USER
+            mkdir -p "$USER_HOME/projects"
+            ln -sfT "/project/$GROUP" "$USER_HOME/projects/$GROUP"
+
+            chgrp $USERNAME "$USER_HOME/projects"
+            chown $USERNAME $PRO_USER
+            chmod 0755 "$USER_HOME/projects"
+            chmod 2700 $PRO_USER
+            restorecon -F -R /project/$GROUP/$USERNAME
+        done
+        /opt/software/slurm/bin/sacctmgr add user ${USERNAMES} Account=${GROUP} -i
+
+    elif [[ "$OP" == "DEL" ]]; then
+        # A group has been removed.
+        # Since we do not want to delete any data we only remove the
+        # symlinks and remove the users from the slurm account.
+        USERNAMES=$(/opt/software/slurm/bin/sacctmgr show user withassoc Account=$GROUP -i format=user --noheader -P)
+        /opt/software/slurm/bin/sacctmgr remove user $USERNAMES Account=${GROUP} -i
+        for USERNAME in $USERNAMES; do
+            USER_HOME="/mnt/home/$USERNAME"
+            rm "$USER_HOME/projects/$GROUP"
+        done
+    fi
+
+    PREV_CONN="$CONN"
 done
