866ec27bfcd7d89e057632ccf828ebd6f613ccae
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-03-23 12:14:48 -0400

    Replace site.pp role based on hostname by tags from Terraform (#116)

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,74 +2,64 @@ stage { ['first', 'second'] : }
 Stage['first'] -> Stage['second'] -> Stage['main']
 
 node default {
-  include profile::consul::client
-  include profile::base
-  include profile::rsyslog::client
-  include profile::freeipa::client
-  include profile::metrics::exporter
-}
+  $instance_tags = lookup("terraform.instances.${::hostname}.tags")
 
-node /^login\d+$/ {
-  include profile::consul::client
-  include profile::base
-  include profile::metrics::exporter
-  include profile::fail2ban
-  include profile::cvmfs::client
-  include profile::rsyslog::client
-  include profile::slurm::submitter
-  include profile::globus::base
-  include profile::singularity
-  include profile::jupyterhub::hub
-  include profile::reverse_proxy
-  include profile::nfs::client
-  include profile::freeipa::client
-  include profile::mfa::login
-}
+  if 'puppet' in $instance_tags {
+    include profile::consul::server
+  } else {
+    include profile::consul::client
+  }
 
-node /^mgmt1$/ {
-  include profile::consul::server
-  include profile::metrics::exporter
-  include profile::freeipa::server
-  include profile::base
-  include profile::nfs::server
-
-  include profile::metrics::server
-  include profile::rsyslog::server
-  include profile::squid::server
-  include profile::slurm::controller
-
-  include profile::freeipa::mokey
-  include profile::slurm::accounting
-  include profile::workshop::mgmt
-  include profile::mfa::mgmt
-
-  include profile::accounts
-  include profile::accounts::guests
-}
-
-node /^mgmt(?:[2-9]|[1-9]\d\d*)$/ {
-  include profile::consul::client
-  include profile::slurm::controller
-  include profile::base
-  include profile::rsyslog::client
-  include profile::freeipa::client
-  include profile::metrics::exporter
-  include profile::mfa::mgmt
-}
-
-node /^[a-z0-9-]*node\d+$/ {
-  include profile::consul::client
   include profile::base
   include profile::metrics::exporter
-  include profile::rsyslog::client
-  include profile::cvmfs::client
-  include profile::gpu
-  include profile::singularity
-  include profile::jupyterhub::node
 
-  include profile::nfs::client
-  include profile::slurm::node
-  include profile::freeipa::client
-  include profile::mfa::node
+  if 'login' in $instance_tags {
+    include profile::fail2ban
+    include profile::cvmfs::client
+    include profile::slurm::submitter
+    include profile::singularity
+    include profile::mfa::login
+  }
 
+  if 'mgmt' in $instance_tags {
+    include profile::freeipa::server
+
+    include profile::metrics::server
+    include profile::rsyslog::server
+    include profile::squid::server
+    include profile::slurm::controller
+
+    include profile::freeipa::mokey
+    include profile::slurm::accounting
+    include profile::workshop::mgmt
+    include profile::mfa::mgmt
+
+    include profile::accounts
+    include profile::accounts::guests
+  } else {
+    include profile::freeipa::client
+    include profile::rsyslog::client
+  }
+
+  if 'node' in $instance_tags {
+    include profile::cvmfs::client
+    include profile::gpu
+    include profile::singularity
+    include profile::jupyterhub::node
+
+    include profile::slurm::node
+    include profile::mfa::node
+  }
+
+  if 'nfs' in $instance_tags {
+    include profile::nfs::server
+  } else {
+    include profile::nfs::client
+  }
+
+  if 'proxy' in $instance_tags {
+    include profile::jupyterhub::hub
+    include profile::reverse_proxy
+    include profile::globus::base
+  }
 }
