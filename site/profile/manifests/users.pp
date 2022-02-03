4a4488df40c85b911649307db12bf1c30687e4c6
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-02-03 15:47:41 -0500

    Set default password as empty

--- site/profile/manifests/users.pp
+++ site/profile/manifests/users.pp
@@ -31,7 +31,7 @@ class profile::users::local (
 
 define profile::users::ldap_user (
   Array[String] $groups,
-  String[8] $passwd = undef,
+  String[8] $passwd = '',
   Array[String] $public_keys = [],
   Integer[0] $count = 1,
   )
