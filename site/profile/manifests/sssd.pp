f0b4044cafc890478a8a9b9e705861c134f05938
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-03-13 00:14:50 -0500

    Add configuration of ldap.conf if tls reqcert is demand

--- site/profile/manifests/sssd.pp
+++ site/profile/manifests/sssd.pp
@@ -3,6 +3,8 @@ class profile::sssd::client(
 ){
   require profile::freeipa::client
 
+  package { 'sssd-ldap': }
+
   $domain_name = lookup('profile::freeipa::base::domain_name')
   $ipa_domain = "int.${domain_name}"
 
@@ -18,6 +20,25 @@ class profile::sssd::client(
     }),
     seltype => 'sssd_conf_t',
     notify  => Service['sssd']
+  }
+
+  $domains.map | $key, $values | {
+    if('ldap_tls_reqcert' in $values and $values['ldap_tls_reqcert'] in ['demand', 'hard']){
+      $uris = join($values['ldap_uri'], ' ')
+      $ldap_conf_template =  @("EOT")
+# Managed by puppet
+SASL_NOCANON    on
+URI ${uris}
+BASE ${values['ldap_search_base']}
+EOT
+      file {'/etc/openldap/ldap.conf':
+        content => $ldap_conf_template,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+      }
+      break()
+    }
   }
 
   service { 'sssd':
