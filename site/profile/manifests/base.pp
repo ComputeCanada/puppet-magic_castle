2fd5e1aac7e366cc2b621f5ca835a2bea6c4cc8e
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-08-30 11:07:03 -0400

    Stop and disable consul in prepare4image

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -16,8 +16,10 @@ class profile::base (
       #!/bin/bash -e
       systemctl stop puppet
       systemctl stop slurmd &> /dev/null || true
+      systemctl stop consul &> /dev/null || true
       systemctl disable puppet
       systemctl disable slurmd &> /dev/null || true
+      systemctl disable consul &> /dev/null || true
       /sbin/ipa-client-install -U --uninstall
       rm -rf /etc/puppetlabs
       cloud-init clean --logs
