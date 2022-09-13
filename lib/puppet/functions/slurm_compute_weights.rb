8241a0174d26c1ccbed4a84a70e184ca37daa621
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-09-13 17:00:36 -0400

    Fix slurm_compute_weights sorted_specs

--- lib/puppet/functions/slurm_compute_weights.rb
+++ lib/puppet/functions/slurm_compute_weights.rb
@@ -7,7 +7,7 @@ Puppet::Functions.create_function(:slurm_compute_weights) do
     def slurm_compute_weights(instances)
         require 'set'
         unique_specs = Set.new(instances.values.map {|i| i['specs']})
-        sorted_specs = unique_specs.sort_by{|spec| [spec['gpu'], spec['realmemory'], spec['cpus']]}
+        sorted_specs = unique_specs.sort_by{|spec| [spec['gpus'], spec['realmemory'], spec['cpus']]}
         weights = Hash.new
         for i in 0..sorted_specs.size-1
             weights[sorted_specs[i]] = i+1
