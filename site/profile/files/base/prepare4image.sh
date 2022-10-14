05343ccf8bc250817622fe009212cbbf1e3814d9
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-10-14 14:56:36 -0400

    Implement sysprep ssh-hostkeys in prepare4image.sh

--- site/profile/files/base/prepare4image.sh
+++ site/profile/files/base/prepare4image.sh
@@ -25,6 +25,9 @@ cloud-init clean --logs
 # sysprep kerberos-hostkeytab
 rm -f /etc/krb5.keytab
 
+# sysprep ssh-hostkeys
+rm -f /etc/ssh/*_host_*
+
 # sysprep ssh-userdir
 find / -maxdepth 2 -name .ssh -type d -exec rm -rf {} \;
 
