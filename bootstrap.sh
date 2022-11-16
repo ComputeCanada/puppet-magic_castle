e73a94e12c55b4bc3e34cbbff80bf35c56d0d103
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-11-16 16:15:21 -0500

    Define a specific password for directory server
    Make it part of the bootstrap process and make sure it can be change
    by replacing its value in hieradata.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ PATH=$PATH:/opt/puppetlabs/puppet/bin
     eyaml encrypt -l 'profile::slurm::base::munge_key' -o block -s $(openssl rand 1024 | openssl enc -A -base64) --pkcs7-public-key=/etc/puppetlabs/puppet/eyaml/boot_public_key.pkcs7.pem
     eyaml encrypt -l 'profile::slurm::accounting::password' -o block -s $(openssl rand -base64 9) --pkcs7-public-key=/etc/puppetlabs/puppet/eyaml/boot_public_key.pkcs7.pem
     eyaml encrypt -l 'profile::freeipa::mokey::password' -o block -s $(openssl rand -base64 9) --pkcs7-public-key=/etc/puppetlabs/puppet/eyaml/boot_public_key.pkcs7.pem
+    eyaml encrypt -l 'profile::freeipa::server::ds_password' -o block -s $(openssl rand -base64 9) --pkcs7-public-key=/etc/puppetlabs/puppet/eyaml/boot_public_key.pkcs7.pem
 ) >> /etc/puppetlabs/code/environments/production/data/bootstrap.yaml
 
 # Check if the puppet module for consul is present
