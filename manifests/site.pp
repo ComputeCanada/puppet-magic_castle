19ca55b542a8fe88c8de1a43732b3141ea07b85c
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-07-03 13:26:50 -0400

    Replace site.pp by site.yaml

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,78 +1,15 @@
-stage { ['first', 'second']: }
-Stage['first'] -> Stage['second'] -> Stage['main']
-
 node default {
   $instance_tags = lookup("terraform.instances.${facts['networking']['hostname']}.tags")
 
-  if 'puppet' in $instance_tags {
-    include profile::consul::server
-  } else {
-    include profile::consul::client
+  include(lookup('magic_castle::site::all', undef, undef, []))
+
+  $instance_tags.each | $tag | {
+    include(lookup("magic_castle::site::tags.${tag}", undef, undef, []))
   }
-
-  include profile::base
-  include profile::users::local
-  include profile::sssd::client
-  include profile::metrics::node_exporter
-
-  if 'login' in $instance_tags {
-    include profile::fail2ban
-    include profile::cvmfs::client
-    include profile::slurm::submitter
-    include profile::ssh::hostbased_auth::client
-  }
-
-  if 'mgmt' in $instance_tags {
-    include profile::freeipa::server
-
-    include profile::metrics::server
-    include profile::metrics::slurm_exporter
-    include profile::rsyslog::server
-    include profile::squid::server
-    include profile::slurm::controller
-
-    include profile::freeipa::mokey
-    include profile::slurm::accounting
-
-    include profile::accounts
-    include profile::users::ldap
-  } else {
-    include profile::freeipa::client
-    include profile::rsyslog::client
-  }
-
-  if 'node' in $instance_tags {
-    include profile::cvmfs::client
-    include profile::gpu
-    include profile::jupyterhub::node
-
-    include profile::slurm::node
-    include profile::ssh::hostbased_auth::client
-    include profile::ssh::hostbased_auth::server
-
-    include profile::metrics::slurm_job_exporter
-
-    Class['profile::nfs::client'] -> Service['slurmd']
-    Class['profile::gpu'] -> Service['slurmd']
-  }
-
-  if 'nfs' in $instance_tags {
-    include profile::nfs::server
-    include profile::cvmfs::alien_cache
-  } else {
-    include profile::nfs::client
-  }
-
-  if 'proxy' in $instance_tags {
-    include profile::jupyterhub::hub
-    include profile::reverse_proxy
-  }
-
-  if 'dtn' in $instance_tags {
-    include profile::globus
-  }
-
-  if 'mfa' in $instance_tags {
-    include profile::mfa
+  $not_tags = lookup('magic_castle::site::not_tags')
+  $not_tags.each | $tag, $classes | {
+    if ! ($tag in $instance_tags) {
+      include($classes)
+    }
   }
 }
