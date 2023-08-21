8acfbebcb3d8de762322f0d5e861799f1af1a835
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-08-21 15:14:35 -0400

    Add command to upgrade portal venv pip version

--- site/profile/manifests/userportal.pp
+++ site/profile/manifests/userportal.pp
@@ -213,6 +213,18 @@ class profile::userportal::install_tarball (String $version) {
     require => Package['python38'],
   }
 
+  exec { 'userportal_upgrade_pip':
+    command     => 'pip3 install --upgrade pip',
+    path        => [
+      '/opt/software/userportal-env/bin',
+      '/usr/bin',
+    ],
+    refreshonly => true,
+    subscribe   => [
+      Exec['userportal_venv'],
+    ],
+  }
+
   file { '/var/www/userportal/':
     ensure => 'directory',
     owner  => 'apache',
@@ -244,6 +256,7 @@ class profile::userportal::install_tarball (String $version) {
     ],
     require     => [
       Exec['userportal_venv'],
+      Exec['userportal_upgrade_pip'],
       Package['python38-devel'],
       Package['mariadb-devel'],
       Package['openldap-devel'],
@@ -258,6 +271,9 @@ class profile::userportal::install_tarball (String $version) {
       '/usr/bin',
     ],
     creates => '/opt/software/userportal-env/lib/python3.8/site-packages/django_pam/__init__.py',
-    require => [Exec['userportal_venv']],
+    require => [
+      Exec['userportal_venv'],
+      Exec['userportal_upgrade_pip'],
+    ],
   }
 }
