e60a878b8fe6d0531f87dbac3887065381965b5c
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-09-18 16:10:54 -0400

    Improve prepare4image.sh script

--- site/profile/files/base/prepare4image.sh
+++ site/profile/files/base/prepare4image.sh
@@ -9,9 +9,17 @@ systemctl disable consul &> /dev/null || true
 systemctl disable consul-template &> /dev/null || true
 /sbin/ipa-client-install -U --uninstall
 rm -rf /etc/puppetlabs
+rm -rf /opt/puppetlabs/puppet/cache
 rm /opt/consul/node-id /opt/consul/checkpoint-signature /opt/consul/serf/local.snapshot
-grep nfs /etc/fstab | cut -f 2 | xargs umount
-sed -i '/nfs/d' /etc/fstab
+
+# Unmount filesystems and turn off swap
+grep -v -P '(ext4|xfs|vfat|swap|^#|^$)' /etc/fstab | cut -f 2 | xargs umount
+grep -P '(ext4|xfs|vfat|^#|^$)' /etc/fstab > /etc/fstab.new
+swapoff -a
+rm -f /mnt/swap
+mv -f /etc/fstab.new /etc/fstab
+systemctl daemon-reload
+
 systemctl stop syslog
 : > /var/log/messages
 if [ -f /etc/cloud/cloud-init.disabled ]; then
@@ -21,4 +29,15 @@ if [ -f /etc/cloud/cloud-init.disabled ]; then
   systemctl disable cloud-init
 fi
 cloud-init clean --logs
+rm -rf /var/lib/cloud
+rm -f /etc/hostname
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+cat /dev/null > /etc/sysconfig/network
+cat /dev/null > /etc/machine-id
+cat > /etc/sysconfig/network-scripts/ifcfg-eth0 << EOF
+DEVICE=eth0
+TYPE=Ethernet
+ONBOOT=yes
+BOOTPROTO=dhcp
+EOF
 halt -p
