4acf765f6589ba880a3afa73d6d9302d16e3ade4
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-09-19 00:24:09 -0400

    Add log cleaning

--- site/profile/files/base/prepare4image.sh
+++ site/profile/files/base/prepare4image.sh
@@ -7,7 +7,12 @@ systemctl disable puppet
 systemctl disable slurmd &> /dev/null || true
 systemctl disable consul &> /dev/null || true
 systemctl disable consul-template &> /dev/null || true
+
 /sbin/ipa-client-install -U --uninstall
+rm -f /var/log/ipaclient-uninstall.log
+rm -f /var/log/ipaclient-install.log
+rm -rf /etc/sssd/sssd.conf.deleted
+
 rm -rf /etc/puppetlabs
 rm -rf /opt/puppetlabs/puppet/cache
 rm /opt/consul/node-id /opt/consul/checkpoint-signature /opt/consul/serf/local.snapshot
@@ -22,6 +27,11 @@ systemctl daemon-reload
 
 systemctl stop syslog
 : > /var/log/messages
+: > /var/log/munge/munged.log
+: > /var/log/secure
+: > /var/log/cron
+: > /var/log/audit/audit.log
+
 if [ -f /etc/cloud/cloud-init.disabled ]; then
   # This is for GCP where we install cloud-init on first boot
   rm /etc/cloud/cloud-init.disabled
@@ -32,8 +42,8 @@ cloud-init clean --logs
 rm -rf /var/lib/cloud
 rm -f /etc/hostname
 rm -f /etc/udev/rules.d/70-persistent-net.rules
-cat /dev/null > /etc/sysconfig/network
-cat /dev/null > /etc/machine-id
+: > /etc/sysconfig/network
+: > /etc/machine-id
 cat > /etc/sysconfig/network-scripts/ifcfg-eth0 << EOF
 DEVICE=eth0
 TYPE=Ethernet
