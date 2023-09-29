8d40ffb3a55224be8ae53dc52ffeb12e2ec293da
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-09-29 14:30:27 -0400

    Fix memory comparison to identify VGPU in nvidia_grid_vgpu.sh

--- site/profile/facts.d/nvidia_grid_vgpu.sh
+++ site/profile/facts.d/nvidia_grid_vgpu.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # NVIDIA Vendor ID is : 0x10de
-# Grep the GPU memory size from the product description
-GPU_MEM_SIZE=$(/usr/sbin/lspci -d 10de: | grep -oP '\[*\K[0-9]*[T|G](?=B)')
-# Grep the actual GPU memory size from lspci verbose
-AVAIL_MEM_SIZE=$(/usr/sbin/lspci -d 10de: -v | grep -oP 'Memory.*\(64-bit, prefetchable\).*\[size=\K([0-9]*[T|G])(?=])')
+# Grep the GPU memory size from the product description and convert to bytes
+GPU_MEM_SIZE=$(/usr/sbin/lspci -d 10de: | grep -oP '\[*\K[0-9]*[T|G](?=B)' | numfmt --from=iec)
+# Grep the actual GPU memory size from lspci verbose and convert to bytes
+AVAIL_MEM_SIZE=$(/usr/sbin/lspci -d 10de: -v | grep -oP 'Memory.*\(64-bit, prefetchable\).*\[size=\K([0-9]*[T|G])(?=])' | numfmt --from=iec)
 
-# If the memory available from the product description is different than the available memory
+# If the memory available from the product description is greater than the available memory
 # we conclude it must be a virtual GPU
 if [[ ! -z "${GPU_MEM_SIZE}" ]] && [[ ! -z "${AVAIL_MEM_SIZE}" ]]; then
-    IS_VGPU=$(test "${GPU_MEM_SIZE}" != "${AVAIL_MEM_SIZE}" && echo true || echo false)
+    IS_VGPU=$(test "${GPU_MEM_SIZE}" -gt "${AVAIL_MEM_SIZE}" && echo true || echo false)
 else
     IS_VGPU='false'
 fi
