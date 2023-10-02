2a909705006009eaf4b01ca679e0146de5bcda3d
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-10-02 08:55:03 -0400

    Limit grep output to one match in nvidia_grid_vgpu

--- site/profile/facts.d/nvidia_grid_vgpu.sh
+++ site/profile/facts.d/nvidia_grid_vgpu.sh
@@ -2,9 +2,9 @@
 
 # NVIDIA Vendor ID is : 0x10de
 # Grep the GPU memory size from the product description and convert to bytes
-GPU_MEM_SIZE=$(/usr/sbin/lspci -d 10de: | grep -oP '\[*\K[0-9]*[T|G](?=B)' | numfmt --from=iec)
+GPU_MEM_SIZE=$(/usr/sbin/lspci -d 10de: | grep -m1 -oP '\[*\K[0-9]*[T|G](?=B)' | numfmt --from=iec)
 # Grep the actual GPU memory size from lspci verbose and convert to bytes
-AVAIL_MEM_SIZE=$(/usr/sbin/lspci -d 10de: -v | grep -oP 'Memory.*\(64-bit, prefetchable\).*\[size=\K([0-9]*[T|G])(?=])' | numfmt --from=iec)
+AVAIL_MEM_SIZE=$(/usr/sbin/lspci -d 10de: -v | grep -m1 -oP 'Memory.*\(64-bit, prefetchable\).*\[size=\K([0-9]*[T|G])(?=])' | numfmt --from=iec)
 
 # If the memory available from the product description is greater than the available memory
 # we conclude it must be a virtual GPU
