4221fc9ba76ed1217957ad5e03617e89840ab370
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2024-01-25 11:28:56 -0500

    Generalize ceph.pp to allow multiple cephfs mounting

--- site/profile/manifests/ceph.pp
+++ site/profile/manifests/ceph.pp
@@ -1,62 +1,30 @@
-class profile::ceph::client (
-  String $share_name,
-  String $access_key,
-  String $export_path,
-  Array[String] $mon_host,
-  Array[String] $mount_binds = [],
-  String $mount_name = 'cephfs01',
-  String $binds_fcontext_equivalence = '/home',
-) {
-  class { 'profile::ceph::client::config':
-    share_name  => $share_name,
-    access_key  => $access_key,
-    export_path => $export_path,
-    mon_host    => $mon_host,
+type CephFS = Struct[
+  {
+    'access_key' => String,
+    'export_path' => String,
+    'mount_binds' => Optional[Array[Tuple[String, String]]],
+    'binds_fcontext_equivalence' => Optional[String],
   }
+]
 
-  file { "/mnt/${mount_name}":
-    ensure => directory,
-  }
+class profile::ceph::client (
+  Array[String] $mon_host,
+  Hash[String, CephFS] $shares,
+) {
+  require profile::ceph::client::install
 
   $mon_host_string = join($mon_host, ',')
-  mount { "/mnt/${mount_name}":
-    ensure  => 'mounted',
-    fstype  => 'ceph',
-    device  => "${mon_host_string}:${export_path}",
-    options => "name=${share_name},secretfile=/etc/ceph/client.keyonly.${share_name}",
-    require => Class['profile::ceph::client::config'],
+  $ceph_conf = @("EOT")
+    [client]
+    client quota = true
+    mon host = ${mon_host_string}
+    | EOT
+
+  file { '/etc/ceph/ceph.conf':
+    content => $ceph_conf,
   }
 
-  $mount_binds.each |$mount| {
-    file { "/mnt/${mount_name}/${mount}":
-      ensure  => directory,
-      require => Class['profile::ceph::client::config'],
-    }
-    file { "/${mount}":
-      ensure  => directory,
-      require => Class['profile::ceph::client::config'],
-    }
-    mount { "/${mount}":
-      ensure  => 'mounted',
-      fstype  => 'none',
-      options => 'rw,bind',
-      device  => "/mnt/${mount_name}/${mount}",
-      require => [
-        File["/mnt/${mount_name}/${mount}"],
-        File["/${mount}"],
-      ],
-    }
-
-    if ($binds_fcontext_equivalence != '' and "/${mount}" != $binds_fcontext_equivalence) {
-      selinux::fcontext::equivalence { "/${mount}":
-        ensure  => 'present',
-        target  => $binds_fcontext_equivalence,
-        require => Mount["/${mount}"],
-        notify  => Selinux::Exec_restorecon["/${mount}"],
-      }
-      selinux::exec_restorecon { "/${mount}": }
-    }
-  }
+  ensure_resources(profile::ceph::client::share, $shares, { 'mount_binds' => [], 'binds_fcontext_equivalence' => '/home' })
 }
 
 class profile::ceph::client::install {
@@ -90,41 +58,69 @@ class profile::ceph::client::install {
   }
 }
 
-class profile::ceph::client::config (
-  String $share_name,
+define profile::ceph::client::share (
   String $access_key,
   String $export_path,
-  Array[String] $mon_host,
+  Array[Tuple[String, String]] $mount_binds,
+  String $binds_fcontext_equivalence,
 ) {
-  require profile::ceph::client::install
-
   $client_fullkey = @("EOT")
-    [client.${share_name}]
+    [client.${name}]
     key = ${access_key}
     | EOT
 
-  file { "/etc/ceph/client.fullkey.${share_name}":
+  file { "/etc/ceph/client.fullkey.${name}":
     content => $client_fullkey,
     mode    => '0600',
     owner   => 'root',
     group   => 'root',
   }
 
-  file { "/etc/ceph/client.keyonly.${share_name}":
+  file { "/etc/ceph/client.keyonly.${name}":
     content => Sensitive($access_key),
     mode    => '0600',
     owner   => 'root',
     group   => 'root',
   }
 
-  $mon_host_string = join($mon_host, ',')
-  $ceph_conf = @("EOT")
-    [client]
-    client quota = true
-    mon host = ${mon_host_string}
-    | EOT
+  file { "/mnt/${name}":
+    ensure => directory,
+  }
 
-  file { '/etc/ceph/ceph.conf':
-    content => $ceph_conf,
+  $mon_host_string = join($mon_host, ',')
+  mount { "/mnt/${name}":
+    ensure  => 'mounted',
+    fstype  => 'ceph',
+    device  => "${mon_host_string}:${export_path}",
+    options => "name=${name},secretfile=/etc/ceph/client.keyonly.${name}",
+    require => File['/etc/ceph/ceph.conf'],
+  }
+
+  $mount_binds.each |$tuple| {
+    $src = $tuple[0]
+    $dst = $tuple[1]
+    file { "/${dst}":
+      ensure  => directory,
+    }
+    mount { "/${dst}":
+      ensure  => 'mounted',
+      fstype  => 'none',
+      options => 'rw,bind',
+      device  => "/mnt/${name}/${src}",
+      require => [
+        File["/${dst}"],
+        Mount["/mnt/${name}"]
+      ],
+    }
+
+    if ($binds_fcontext_equivalence != '' and $binds_fcontext_equivalence != "/${dst}") {
+      selinux::fcontext::equivalence { "/${dst}":
+        ensure  => 'present',
+        target  => $binds_fcontext_equivalence,
+        require => Mount["/${dst}"],
+        notify  => Selinux::Exec_restorecon["/${dst}"],
+      }
+      selinux::exec_restorecon { "/${dst}": }
+    }
   }
 }
