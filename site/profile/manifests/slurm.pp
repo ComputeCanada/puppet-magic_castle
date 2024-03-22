5bbb0a40339a8a8aa7ccefb56ea2d1770e489100
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2024-03-22 12:21:03 -0400

    Use ephemeral0 as slurmd localscratch when present

--- site/profile/manifests/slurm.pp
+++ site/profile/manifests/slurm.pp
@@ -693,6 +693,17 @@ class profile::slurm::node {
   }
 
   ensure_resource('file', '/localscratch', { 'ensure' => 'directory', 'seltype' => 'tmp_t' })
+  if '/dev/disk/by-label/ephemeral0' in $facts['/dev/disk'] {
+    mount { '/localscratch':
+      ensure  => mounted,
+      device  => "/mnt/ephemeral0",
+      fstype  => none,
+      options => 'rw,bind',
+      require => [
+        File['/localscratch'],
+      ],
+    }
+  }
 
   file { '/var/spool/slurmd':
     ensure => 'directory',
