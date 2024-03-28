11f30fd478d2a7346b1235fe37cc472158f67e29
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2024-03-28 12:28:10 -0400

    Add support for commas in ssh options

--- site/profile/manifests/users.pp
+++ site/profile/manifests/users.pp
@@ -150,7 +150,7 @@ define profile::users::local_user (
     $key_value = $split[$key_type_index+1]
 
     if $key_type_index != 0 {
-      $key_options = $split[0, $key_type_index].join(' ').split(',')
+      $key_options = ssh_split_options($split[0, $key_type_index].join(' '))
     } else {
       $key_options = undef
     }
