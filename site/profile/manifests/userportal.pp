e4d1fb7b925a6f9123f0adfbe5f8aa3e17124394
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2024-04-12 12:12:27 -0400

    Fix trailblazing_turtle declaration

--- site/profile/manifests/userportal.pp
+++ site/profile/manifests/userportal.pp
@@ -5,11 +5,19 @@ class profile::userportal {
   $domain_name = lookup('profile::freeipa::base::domain_name')
   $int_domain_name = "int.${domain_name}"
   $base_dn = join(split($int_domain_name, '[.]').map |$dc| { "dc=${dc}" }, ',')
-  $admin_password = lookup('profile::freeipa::server::admin_password')
 
   class { 'trailblazing_turtle':
-    logins      => $logins,
-    base_dn     => $base_dn,
-    domain_name => $domain_name,
+    root_api_token  => lookup('trailblazing_turtle::server::root_api_token'),
+    password        => lookup('trailblazing_turtle::server::password'),
+    prometheus_ip   => lookup('trailblazing_turtle::server::prometheus_ip'),
+    prometheus_port => lookup('trailblazing_turtle::server::prometheus_port'),
+    db_ip           => lookup('trailblazing_turtle::server::db_ip'),
+    db_port         => lookup('trailblazing_turtle::server::db_port'),
+    ldap_password   => lookup('trailblazing_turtle::server::ldap_password'),
+    slurm_password  => lookup('trailblazing_turtle::server::slurm_password'),
+    cluster_name    => lookup('trailblazing_turtle::server::cluster_name'),
+    logins          => $logins,
+    base_dn         => $base_dn,
+    domain_name     => $domain_name,
   }
 }
