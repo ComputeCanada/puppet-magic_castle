073c31c2895fcb9e142da47d2aa9355d63305ad3
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2025-03-21 10:08:02 -0400

    Refactor puppetserver alias

--- site/profile/manifests/puppetserver.pp
+++ site/profile/manifests/puppetserver.pp
@@ -1,10 +1,13 @@
 class profile::puppetserver {
+  $eyaml_path = '/opt/puppetlabs/puppet/bin/eyaml'
+  $boot_private_key_path = '/etc/puppetlabs/puppet/eyaml/boot_private_key.pkcs7.pem'
+  $boot_eyaml = '/etc/puppetlabs/code/environments/production/data/bootstrap.yaml'
   $local_users = lookup('profile::users::local::users', undef, undef, {})
   $local_users.each | $user, $attrs | {
     if pick($attrs['sudoer'], false) {
       file_line { "${user}_eyamlbootstrap":
         path => "/${user}/.bashrc",
-        line => 'alias eyamlbootstrap="sudo /opt/puppetlabs/puppet/bin/eyaml decrypt --pkcs7-private-key /etc/puppetlabs/puppet/eyaml/boot_private_key.pkcs7.pem -f /etc/puppetlabs/code/environments/production/data/bootstrap.yaml | less"'
+        line => "alias eyamlbootstrap=\"sudo ${eyaml_path} decrypt --pkcs7-private-key ${boot_private_key_path} -f ${boot_eyaml} | less\"",
       }
     }
   }
