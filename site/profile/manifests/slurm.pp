e93ec43ae5473ea24fad1f898b1bb1ea32f7c5c0
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2025-10-17 15:22:41 -0400

    Bring back restarting slurmd
    This time with better filtering on state instead of just
    whenever it is not idle.

--- site/profile/manifests/slurm.pp
+++ site/profile/manifests/slurm.pp
@@ -761,6 +761,21 @@ class profile::slurm::node (
     ],
   }
 
+  # If the Slurm SuspendProgram has failed for any reason
+  # during a node power off, it is possible that the node will
+  # still be online, with slurmd running, but the controller will
+  # ignore it until slurmd is restarted. This exec check if the
+  # controller thinks the node is powered off or non responsive
+  # and if it is the case, it restarts slurmd so the state in
+  # in slurmctld can be properly refreshed.
+  $hostname = $facts['networking']['hostname']
+  exec { 'slurmd_state_invalid_restart':
+    command => 'systemctl restart slurmd',
+    onlyif  => "test $(sinfo -h --states=no_respond,powered_down -o %n -n ${hostname} | wc -l) -eq 1",
+    path    => ['/usr/bin', '/opt/software/slurm/bin'],
+    require => Service['slurmd'],
+  }
+
   logrotate::rule { 'slurmd':
     path         => '/var/log/slurm/slurmd.log',
     rotate       => 5,
