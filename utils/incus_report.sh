15fe163aba6adb979c9130418082893367723ca1
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-01-30 14:24:38 -0500

    Fix output format

--- utils/incus_report.sh
+++ utils/incus_report.sh
@@ -14,8 +14,7 @@ for nodename in $(incus list -c n -f csv); do
 		echo " FAILED"
 		cat <<- EOF
 			#### failures
-			<details><pre><code>
-			$(incus exec $nodename -- journalctl -u puppet -p3..4 | grep -i -v -P '(connection|routes|wrapped)')
+			<details><pre><code>$(incus exec $nodename -- journalctl -u puppet -p3..4 | grep -i -v -P '(connection|routes|wrapped)')
 			</code></pre></details>
 		EOF
 		SUCCESSFUL=1
@@ -23,17 +22,17 @@ for nodename in $(incus list -c n -f csv); do
 		echo
 	fi
 
+	echo
 	cat <<- EOF
 		#### successes
-		<details><pre><code>
-		$(incus exec $nodename -- journalctl -u puppet -p5..5)
+		<details><pre><code>$(incus exec $nodename -- journalctl -u puppet -p5..5)
 		</code></pre></details>
 	EOF
 
+	echo
 	cat <<- EOF
-		#### Puppet report"
-		<details><pre><code>
-		$(incus exec mgmt1 -- cat /var/lib/node_exporter/puppet_report_${nodename}.prom | grep '^puppet_report')
+		#### Puppet report
+		<details><pre><code>$(incus exec mgmt1 -- cat /var/lib/node_exporter/puppet_report_${nodename}.prom | grep '^puppet_report')
 		</code></pre></details>
 	EOF
 done
