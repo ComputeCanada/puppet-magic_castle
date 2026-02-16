55f86bd162afde855343a7dcbb9a8b70601b2e99
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-02-16 09:37:52 -0500

    Make lookup cidr from terraform data optional

--- site/profile/functions/getcidr.pp
+++ site/profile/functions/getcidr.pp
@@ -1,4 +1,8 @@
 function profile::getcidr() >> String {
+  $terraform_cidr = lookup('terraform.network.cidr', String, 'first', '')
+  if $terraform_cidr != '' {
+    return $terraform_cidr
+  }
   $interface = profile::getlocalinterface()
   $masklen = extlib::netmask_to_cidr(profile::getnetmask())
   $ip = $networking['interfaces'][$interface]['ip']
