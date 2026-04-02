d1869497ee1d87f22ac41f880d1c054fc9c4730c
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-04-02 13:35:44 -0400

    allow to split metrix VM from slurm/database VM

--- site/profile/manifests/metrix.pp
+++ site/profile/manifests/metrix.pp
@@ -15,6 +15,12 @@ class profile::metrix {
   $int_domain_name = lookup('profile::freeipa::base::ipa_domain')
   $base_dn = join(split($int_domain_name, '[.]').map |$dc| { "dc=${dc}" }, ',')
 
+  if lookup('terraform.tag_ip.metrix.0') != lookup('terraform.tag_ip.mgmt.0') {
+    $slurm_db_ip = lookup('terraform.tag_ip.mgmt.0')
+  }
+  else {
+    $slurm_db_ip = '127.0.0.1'
+  }
   class { 'metrix':
     root_api_token  => lookup('metrix::root_api_token'),
     password        => lookup('metrix::password'),
@@ -23,12 +29,16 @@ class profile::metrix {
     db_ip           => lookup('metrix::db_ip'),
     db_port         => lookup('metrix::db_port'),
     ldap_password   => lookup('metrix::ldap_password'),
+    slurm_user      => lookup('metrix::slurm_user'),
     slurm_password  => lookup('metrix::slurm_password'),
+    slurm_db_ip     => $slurm_db_ip,
+    slurm_db_port   => lookup('metrix::slurm_db_port'),
     cluster_name    => lookup('metrix::cluster_name'),
     subdomain       => lookup('metrix::subdomain'),
     logins          => $logins,
     base_dn         => $base_dn,
     domain_name     => $domain_name,
+    auth_type       => lookup('metrix::auth_type')
   }
   Class['metrix'] ~> Service['httpd']
 }
