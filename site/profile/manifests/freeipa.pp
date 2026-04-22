f12f436f384bb59251ac8cd3b2c1fa3386117dad
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-04-22 15:05:43 -0400

    Make ipa admin password definition optional

--- site/profile/manifests/freeipa.pp
+++ site/profile/manifests/freeipa.pp
@@ -53,7 +53,7 @@ class profile::freeipa::client (
   include profile::sssd::client
 
   $ipa_domain = lookup('profile::freeipa::base::ipa_domain')
-  $admin_password = lookup('profile::freeipa::server::admin_password')
+  $admin_password = lookup('profile::freeipa::server::admin_password', undef, undef, undef)
   $fqdn = "${facts['networking']['hostname']}.${ipa_domain}"
   $realm = upcase($ipa_domain)
   $ipaddress = lookup('terraform.self.local_ip')
