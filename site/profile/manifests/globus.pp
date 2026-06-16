a638c2c3852665f52e12c73a58a17770c1261e68
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-06-16 14:24:58 -0400

    Simplify globus install

--- site/profile/manifests/globus.pp
+++ site/profile/manifests/globus.pp
@@ -2,16 +2,6 @@ class profile::globus {
   package { 'wget':
     ensure => installed,
   }
-
-  $public_ip = lookup('terraform.self.public_ip')
-  class { 'globus':
-    display_name  => $globus::display_name,
-    client_id     => $globus::client_id,
-    client_secret => $globus::client_secret,
-    contact_email => $globus::contact_email,
-    ip_address    => $public_ip,
-    organization  => $globus::organization,
-    owner         => $globus::owner,
-    require       => Package['wget'],
-  }
+  include globus
+  Package['wget'] -> Class['globus']
 }
