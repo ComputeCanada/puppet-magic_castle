ded5499775fbbb47d2831d52fd5c24fe56bfc2b2
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-06-17 21:16:22 -0400

    Add missing firewall rules to globus profile

--- site/profile/manifests/globus.pp
+++ site/profile/manifests/globus.pp
@@ -4,4 +4,20 @@ class profile::globus {
   }
   include globus
   Package['wget'] -> Class['globus']
+
+  firewall { '200 globus public':
+    chain  => 'INPUT',
+    dport  => [443],
+    proto  => 'tcp',
+    source => '0.0.0.0/0',
+    action => 'accept',
+  }
+
+  firewall { '201 gridftp':
+    chain  => 'INPUT',
+    dport  => '50000:51000',
+    proto  => 'tcp',
+    source => '0.0.0.0/0',
+    action => 'accept',
+  }
 }
