d513642d759390476c0204e6c806edd31180b19c
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-06-19 10:24:52 -0400

    Add file to create gateway and collection

--- site/profile/manifests/globus.pp
+++ site/profile/manifests/globus.pp
@@ -1,4 +1,6 @@
-class profile::globus {
+class profile::globus (
+  Array[String] $domains = ['globus.org']
+) {
   package { 'wget':
     ensure => installed,
   }
@@ -20,4 +22,46 @@ class profile::globus {
     source => '0.0.0.0/0',
     action => 'accept',
   }
+
+  $domain_string = $domains.map|$value| { " --domain ${value}" }.join(' ')
+  file { '/root/globus-gateway-setup':
+    ensure    => 'file',
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0700',
+    show_diff => false,
+    content   => "GCS_CLI_ENDPOINT_ID=$(jq .endpoint_id -r /var/lib/globus-connect-server/info.json) \
+    globus-connect-server -F json storage-gateway create posix \"${lookup('terraform.data.cluster_name')} \
+    gateway\" ${domain_string} > /var/lib/globus-connect-server/gateway.json",
+  }
+
+  file { '/root/globus-collection-setup':
+    ensure    => 'file',
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0700',
+    show_diff => false,
+    content   => "GCS_CLI_ENDPOINT_ID=$(jq .endpoint_id -r /var/lib/globus-connect-server/info.json)\
+    globus-connect-server -F json collection create $(jq -r .id /var/lib/globus-connect-server/gateway.json) / \
+    \"${lookup('terraform.data.cluster_name')} collection\" > /var/lib/globus-connect-server/collection.json",
+  }
+
+  # exec { 'globus-setup-gateway':
+  #   command     => 'sh /root/globus-gateway-setup',
+  #   environment => [
+  #     "GCS_CLI_CLIENT_ID=${globus::client_id}",
+  #     "GCS_CLI_CLIENT_SECRET=${globus::client_secret}",
+  #   ],
+  #   creates     => '/var/lib/globus-connect-server/gateway.json',
+  #   require     => Exec['globus-endpoint-setup'],
+  # }
+  # exec { 'globus-setup-collection':
+  #   command     => 'sh /root/globus-collection-setup',
+  #   environment => [
+  #     "GCS_CLI_CLIENT_ID=${globus::client_id}",
+  #     "GCS_CLI_CLIENT_SECRET=${globus::client_secret}",
+  #   ],
+  #   creates     => '/var/lib/globus-connect-server/collection.json',
+  #   require     => Exec['globus-gateway-setup'],
+  # }
 }
