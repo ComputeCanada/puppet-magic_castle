0f59c3ff91e4dd9e05b89daa6918bd7422eb1e24
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-06-29 11:09:40 -0400

    Start oidc setup

--- site/profile/manifests/globus.pp
+++ site/profile/manifests/globus.pp
@@ -1,6 +1,7 @@
 class profile::globus (
   String[1] $collection_path = '/nfs',
-  Array[String] $domains = ['globus.org']
+  Array[String] $domains = ['globus.org'],
+  Enum['running', 'stopped'] $ensure_oidc = 'stopped',
 ) {
   package { 'wget':
     ensure => installed,
@@ -15,11 +16,14 @@ class profile::globus (
     group     => 'root',
     mode      => '0700',
     show_diff => false,
-    content   => "GCS_CLI_ENDPOINT_ID=$(jq .endpoint_id -r /var/lib/globus-connect-server/info.json) \
-    globus-connect-server -F json storage-gateway create \
-    --use-explicit-host ${lookup('terraform.self.public_ip')} \
-    posix \"${lookup('terraform.data.cluster_name')} \
-    gateway\" ${domain_string} > /var/lib/globus-connect-server/gateway.json\n",
+    content   => epp(
+      'globus/globus-gateway-setup',
+      {
+        'public_ip'     => lookup('terraform.self.public_ip'),
+        'cluster_name'  => lookup('terraform.data.cluster_name'),
+        'domain_string' => $domain_string,
+      }
+    ),
   }
 
   file { '/root/globus-collection-setup':
@@ -28,11 +32,33 @@ class profile::globus (
     group     => 'root',
     mode      => '0700',
     show_diff => false,
-    content   => "GCS_CLI_ENDPOINT_ID=$(jq .endpoint_id -r /var/lib/globus-connect-server/info.json)\
-    globus-connect-server -F json --use-explicit-host ${lookup('terraform.self.public_ip')} \
-    collection create $(jq -r .id /var/lib/globus-connect-server/gateway.json) ${collection_path} \
-    --default-directory '/home/\$USER' \
-    \"${lookup('terraform.data.cluster_name')} collection\" > /var/lib/globus-connect-server/collection.json\n",
+    content   => epp(
+      'globus/globus-collection-setup',
+      {
+        'public_ip'       => lookup('terraform.self.public_ip'),
+        'cluster_name'    => lookup('terraform.data.cluster_name'),
+        'collection_path' => $collection_path,
+      }
+    ),
+  }
+
+  $domain_name = lookup('terraform.data.domain_name')
+  file { '/root/globus-oidc-setup':
+    ensure    => 'file',
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0700',
+    show_diff => false,
+    content   => epp(
+      'globus/globus-oidc-setup',
+      {
+        'domain_name' => $domain_name,
+      }
+    ),
+  }
+
+  if $ensure_oidc == 'stopped' and length($domains) == 0 {
+    fail('Globus requires at least one authentication domain or ensure OIDC server is running  (profile::globus::ensure_oidc: running)')
   }
 
   exec { 'globus-gateway-setup':
@@ -42,7 +68,10 @@ class profile::globus (
       "GCS_CLI_CLIENT_SECRET=${globus::client_secret.unwrap}",
     ],
     unless      => '/bin/test -s /var/lib/globus-connect-server/gateway.json',
-    require     => Exec['globus-endpoint-setup'],
+    require     => [
+      Exec['globus-endpoint-setup'],
+      File['/root/globus-gateway-setup'],
+    ],
   }
 
   exec { 'globus-collection-setup':
@@ -52,8 +81,35 @@ class profile::globus (
       "GCS_CLI_CLIENT_SECRET=${globus::client_secret.unwrap}",
     ],
     unless      => '/bin/test -s /var/lib/globus-connect-server/collection.json',
-    require     => Exec['globus-gateway-setup'],
+    require     => [
+      Exec['globus-gateway-setup'],
+      File['/root/globus-collection-setup'],
+    ],
   }
+
+  if $ensure_oidc == 'running' {
+    exec { 'globus-oidc-setup':
+      command     => '/bin/sh /root/globus-oidc-setup',
+      environment => [
+        "GCS_CLI_CLIENT_ID=${globus::client_id}",
+        "GCS_CLI_CLIENT_SECRET=${globus::client_secret.unwrap}",
+      ],
+      unless      => '/bin/test -s /var/lib/globus-connect-server/oidc.json',
+      require     => [
+        Exec['globus-endpoint-setup'],
+        File['/root/globus-oidc-setup'],
+      ],
+      before      => Exec['globus-gateway-setup'],
+    }
+  }
+
+  # globus-connect-server storage-gateway update posix 7fd7da12-414a-41f8-8d07-5942c88d9cf8 --domain identity.1f30f9.eb38.gaccess.io
   Firewall <| |> -> Exec['globus-endpoint-setup']
   Mount <| |> -> Exec['globus-collection-setup']
+
+  service { 'globus-oidc':
+    ensure  => $ensure_oidc,
+    enable  => $ensure_oidc == 'running',
+    require => Exec['globus-oidc-setup'],
+  }
 }
